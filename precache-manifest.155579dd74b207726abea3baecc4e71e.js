bdd1b6182bcecfa450d921d92000189d4731a273
NelliPestych <NelliPestych@bigmir.net>
2020-07-05 20:03:57 +0300

    Updates

---
+++ precache-manifest.155579dd74b207726abea3baecc4e71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383332046a90786a5cbf78b1f2d6468e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "b25694c0079c28207ef4",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b2bf4c9921ced580578",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25694c0079c28207ef4",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f0ac77e2.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
